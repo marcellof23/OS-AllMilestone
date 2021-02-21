8eaefb6f3d36c2f97890cba88eb1b525f8aad75c
Jesson Yo <Aphostrophy@gmail.com>
2021-02-21 20:59:20 +0700

    Added cls implementation

--- src/kernel.c
+++ src/kernel.c
@@ -1,15 +1,23 @@
+int VIDEO_OFFSET=0x8000;
+int VIDEO_SEGMENT=0xB000;
+int VIDEO_SCREEN_SIZE = 4000;
+
+
 void handleInterrupt21 (int AX, int BX, int CX, int DX);
 void printString(char *string);
 void readString(char *string);
 void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+void printLogo();
+void cls(int displaymode);
 
 int main() {
   char *string = "OMAEWA";
   char *woi  = "WOIIIs";
   makeInterrupt21();
-  handleInterrupt21(2,string,6,0);
+  handleInterrupt21(2,3,0,0);
   handleInterrupt21(0,string,0,0);
   handleInterrupt21(0,woi,0,0);
+
   // handleInterrupt21(2,"ADA",0,0);
   // handleInterrupt21(1,0,0,0);
   // handleInterrupt21(1,0,0,0);
@@ -26,7 +34,7 @@ void handleInterrupt21 (int AX, int BX, int CX, int DX){
       readString(BX);
       break;
     case 0x2:
-      clear(BX,CX);
+      cls(BX);
       break;
     default:
       printString("Invalid interrupt");
@@ -37,13 +45,11 @@ void printString(char *string){
   int i =0;
   while(*(string+i)!= '\0')
   {
-    if(*(string+i) == '\r')
-      interrupt(0x10,0xE00 + '\n',0,0,0);
-    else if(*(string+i)== '\n')
-      interrupt(0x10,0xE00 + '\r',0,0,0);
     interrupt(0x10,0xE00 + *(string+i),0,0,0);
     ++i;
   }
+  interrupt(0x10,0xE00 + '\n',0,0,0);
+  interrupt(0x10,0xE00 + '\r',0,0,0);
 }
 
 void readString(char *string){
@@ -66,7 +72,16 @@ void readString(char *string){
 
 void clear(char *buffer, int length){
   int i;
-  for(i=0;i<length;i++){
-    buffer[i] = 0x00;
-  }
+	for (i = 0; i < length; i++)
+	{
+		buffer[i] = 0x00;
+	}
+}
+
+void cls(int displaymode){
+  interrupt(0x10,displaymode,0,0);
+}
+
+void printLogo(){
+  
 }
