320f14289f5d86d8eb9f7149287fe2ec0e04ed83
DenilsenAxel <denilsenaxel@gmail.com>
2021-02-21 15:47:29 +0800

    read string done but still not gud

--- src/kernel.c
+++ src/kernel.c
@@ -4,8 +4,9 @@ void readString(char *string);
 void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
 
 int main() {
+  char *string = "meki";
   makeInterrupt21();
-  handleInterrupt21(0,1,1,1);
+  handleInterrupt21(1,string,0,0);
   while (1);
 }
 
@@ -23,17 +24,40 @@ void handleInterrupt21 (int AX, int BX, int CX, int DX){
 }
 
 void printString(char *string){
-  interrupt(10,0xff,0xff,0xff);
-  interrupt(10,0xff,0xff,0xff);
-  interrupt(10,0xff,0xff,0xff);
-  interrupt(10,0xff,0xff,0xff);
-  interrupt(10,0xff,0xff,0xff);
-  interrupt(10,0xff,0xff,0xff);
-  interrupt(10,0xff,0xff,0xff);
+  // Menggunakan int 10h AH = 02h dan 09h
+  
+  // int i = 0;
+  // while(*(string+i) != '\0') {
+  //   interrupt(0x10, 0x0200, 0, 0, i);
+  //   interrupt(0x10, 0x0900 + *(string+i), 0x02, 1, 0);
+  //   i++;
+  // }
+  
+  // Menggunakan int 10h AH = 0eh
+
+  int i = 0;
+  while(*(string+i) != '\0') {
+    interrupt(0x10,0x0e00 + *(string+i),0, 0, 0);
+    i++;
+  }
 }
 
 void readString(char *string){
+  int input, i, test;
+  char *testString;
+  i = 0;
+  input = interrupt(0x16, 0x0000, 0, 0, 0);
+  while(input != 0x0d) {
+    interrupt(0x10, 0x0e00 + input, 0, 0, 0);
+    *(testString+i) = input;
+    i++;
+    input = interrupt(0x16, 0x0000, 0, 0, 0);
+  }
+  *(testString+i) = 0x0;
 
+  printString("\n\0");
+
+  printString(testString);
 }
 
 void clear(char *buffer, int length){
