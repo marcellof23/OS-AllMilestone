303cb2e0dc00c7c08bb0e47fcc4d99af011279fe
marcellof23 <01faria.marcello@gmail.com>
2021-03-13 23:14:22 +0700

    update cek string

--- src/kernel.c
+++ src/kernel.c
@@ -140,11 +140,7 @@ void writeSector(char *buffer,int sector) {
   interrupt(0x13, 0x301, buffer, div(sector,36)*0x100 + mod(sector,18) + 1, mod(div(sector,18),2)*0x100);
 }
 
-<<<<<<< HEAD
 void stringCompare(char *string1,char *string2,int panjang)
-=======
-int cekString(char *string1,char *string2,int panjang)
->>>>>>> 4e4f256ce752e9193ec636b92287cd882b080b7e
 {
   int i = 0;
   while(i<panjang)
