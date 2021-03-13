35852a3850cb1781ec95bd3b8159adc5eec875dd
marcellof23 <01faria.marcello@gmail.com>
2021-03-14 01:32:17 +0700

    fix minor bug utilities.h

--- src/utilities.h
+++ src/utilities.h
@@ -5,7 +5,7 @@ int mod(int x, int y);
 
 int div(int x,int y);
 
-void strcmp(char *string1,char *string2,int length);
+int strcmp(char *string1,char *string2,int length);
 
 void strslice(char *string1,char *string2,int start,int end);
 
