3b535a6c3c92b62af24643631f3a8f37c24836e2
Jesson Yo <Aphostrophy@gmail.com>
2021-04-17 21:18:03 +0700

    modified compile and loadfile to accept argv

--- final.sh
+++ final.sh
@@ -1,13 +1,6 @@
 rm output/system.img
 
-nasm ./asm/bootloader.asm -o ./output/bootloader
-dd if=./output/bootloader of=./output/system.img bs=512 count=1 conv=notrunc
-bcc -ansi -c -o ./output/kernel.o ./src/kernel.c
-bcc -ansi -c -o ./output/utilities.o ./src/utilities.c
-bcc -ansi -c -o ./output/shell.o ./src/shell.c
-nasm -f as86 ./asm/kernel.asm -o ./output/kernel_asm.o
-ld86 -o ./output/kernel -d ./output/kernel.o ./output/kernel_asm.o ./output/utilities.o ./output/shell.o
-dd if=./output/kernel of=./output/system.img bs=512 conv=notrunc seek=1
+./compile.sh
 
 dd if=/dev/zero of=./output/map.img bs=512 count=1
 dd if=/dev/zero of=./output/files.img bs=512 count=2
@@ -19,4 +12,4 @@ dd if=./output/map.img of=./output/system.img bs=512 count=1 seek=256 conv=notru
 dd if=./output/files.img of=./output/system.img bs=512 count=2 seek=257 conv=notrunc
 dd if=./output/sectors.img of=./output/system.img bs=512 count=1 seek=259 conv=notrunc
 
-python3 script/loadfile.py
+python3 script/loadfile.py ./misc/a.txt ./misc/ab.txt
--- script/loadfile.py
+++ script/loadfile.py
@@ -1,94 +1,78 @@
 # This script are meant to be run individually, separated from the Operating System
 
 import os
+import sys
 
-with open("./output/system.img", 'rb') as f:
-    system = f.read()
-    system = bytearray(system)
-    print("System length" ,len(system))
-    map = system[256*512:257*512]
-    print("Initial map:")
-    print(map)
-    files = system[257*512:259*512]
-    sector = system[259*512:260*512]
+def loadfile(filepath):
+    with open("./output/system.img", 'rb') as f:
+        system = f.read()
+        system = bytearray(system)
+        map = system[256*512:257*512]
+        files = system[257*512:259*512]
+        sector = system[259*512:260*512]
 
-# filepath = input("Input filepath: ")
-filepath = "./misc/"
-filename = str(input("Masukkan nama file: "))
-filepath += filename
-x = filepath.split("/")
-filename = x[-1]
-print(filename)
+    x = filepath.split("/")
+    filename = x[-1]
 
-with open(filepath, 'rb') as f:
-    fileloaded = f.read()
-    fileloaded = bytearray(fileloaded)
-    print("File length:",len(fileloaded))
+    with open(filepath, 'rb') as f:
+        fileloaded = f.read()
+        fileloaded = bytearray(fileloaded)
 
-sectorneeded = 0
-filechunk = [[0x0 for i in range(512)] for j in range(16)]
+    sectorneeded = 0
+    filechunk = [[0x0 for i in range(512)] for j in range(16)]
 
-for i in range(0,16):
-    filechunk[i] = fileloaded[i*512:(i+1)*512]
-    print("File chunks: ",filechunk[i])
-    print(len(fileloaded[i*512:(i+1)*512]))
-    if(filechunk[i]):
-        sectorneeded+=1
+    for i in range(0,16):
+        filechunk[i] = fileloaded[i*512:(i+1)*512]
+        if(filechunk[i]):
+            sectorneeded+=1
 
-print("Sector needed :" ,sectorneeded)
-emptysector = 0
-filesectorfound = False
-filesectorindex = 0
+    emptysector = 0
+    filesectorfound = False
+    filesectorindex = 0
 
-for i in range(len(map)):
-    if(map[i]==0x0):
-        emptysector+=1
-
-for i in range(64):
-    if(files[i*16:(i+1)*16]==bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')):
-        filesectorfound = True
-        filesectorindex = i
-        break
-
-print("Empty sectors available:",emptysector)
-if(emptysector-256>sectorneeded and filesectorfound):
-    print("Executing load file...")
-    sectors = [0 for i in range(16)]
-    i = 0
-    counter = 0
-    while(i<512 and counter<sectorneeded):
+    for i in range(len(map)):
         if(map[i]==0x0):
-            map[i] = 0xFF #MARK sector used in map
-            sectors[counter] = i #Save list of sectors in sectors
-            counter+=1
-        i+=1
-    print("Final map:")
-    system[256*512:257*512] = map
-    print(system[256*512:257*512])
-    sectorsindex=0
-    for i in range(32):
-        if(sector[i*16:(i+1)*16]==bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')):
-            sectorsindex = i
-            for j in range(16):
-                sector[i*16+j] = sectors[j]
+            emptysector+=1
+
+    for i in range(64):
+        if(files[i*16:(i+1)*16]==bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')):
+            filesectorfound = True
+            filesectorindex = i
             break
-    
-    system[259*512:260*512] = sector
-    print("Final sector sectors:")
-    print(system[259*512:260*512])
-    files[filesectorindex*16] = 0xFF
-    files[filesectorindex*16+1] = sectorsindex
-    for i in range(len(filename)):
-        files[filesectorindex*16+2+i] = ord(filename[i])
-    system[257*512:259*512] = files
-    print("Final files sector:")
-    print(system[257*512:259*512])
-    for i in range(sectorneeded):
-        system[sectors[i]*512:(sectors[i]+1)*512] = bytes(filechunk[i]).ljust(512,b'\0')
 
-    print("System length" ,len(system))
-    with open("./output/system.img", 'wb') as f:
-        f.write(bytes(system))
+    if(emptysector-256>sectorneeded and filesectorfound):
+        sectors = [0 for i in range(16)]
+        i = 0
+        counter = 0
+        while(i<512 and counter<sectorneeded):
+            if(map[i]==0x0):
+                map[i] = 0xFF #MARK sector used in map
+                sectors[counter] = i #Save list of sectors in sectors
+                counter+=1
+            i+=1
+        system[256*512:257*512] = map
+        sectorsindex=0
+        for i in range(32):
+            if(sector[i*16:(i+1)*16]==bytearray(b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')):
+                sectorsindex = i
+                for j in range(16):
+                    sector[i*16+j] = sectors[j]
+                break
+        
+        system[259*512:260*512] = sector
+        files[filesectorindex*16] = 0xFF
+        files[filesectorindex*16+1] = sectorsindex
+        for i in range(len(filename)):
+            files[filesectorindex*16+2+i] = ord(filename[i])
+        system[257*512:259*512] = files
+        for i in range(sectorneeded):
+            system[sectors[i]*512:(sectors[i]+1)*512] = bytes(filechunk[i]).ljust(512,b'\0')
 
-else:
-    print("Not enough memory to load file")
+        with open("./output/system.img", 'wb') as f:
+            f.write(bytes(system))
+
+    else:
+        print("Not enough memory to load file")
+
+for i in range(1,len(sys.argv)):
+    loadfile(sys.argv[i])
