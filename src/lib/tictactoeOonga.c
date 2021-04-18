b0950f2c3bb863199c96f7f343dcf78a80c79cbe
DenilsenAxel <denilsenaxel@gmail.com>
2021-04-18 22:31:07 +0800

    fixed typo

--- src/lib/tictactoeOonga.c
+++ src/lib/tictactoeOonga.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-char square[10] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char square[] = { 'o', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
 int checkwin();
 void board();
