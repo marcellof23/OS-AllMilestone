3e558d59348a3f4c313cebeda742b76b2710ac83
Jesson Yo <Aphostrophy@gmail.com>
2021-04-19 02:27:57 +0700

    nyoba baca temp file

--- src/lib/cat.c
+++ src/lib/cat.c
@@ -1,50 +1,59 @@
+#include "../module/folderIO.h"
+#include "../module/stringutil.h"
+#include "../module/math.h"
+
 void cat(char * filenames,unsigned char dir);
 
 int main(){
+    char buffer[512*16];
     char execStatus[16];
+    clear(buffer,512*16);
     interrupt(0x21,0,"cat dipanggil\r\n",0,0);
+    getArguments(buffer);
+    interrupt(0x21,0,buffer,0,0);
+    interrupt(0x21,0,"\r\n",0,0);
     interrupt(0x21,0x0006,"shell",0x3000,execStatus);
     return 1;
 }
 
-// void cat(char * filenames, unsigned char parentIdx)
-// {
-//     char files[1024];
-//     char buff[512 * 16];
-//     int idx = strlen(filenames);
-//     int pathIdx;
-//     char output[100];
-//     char filematrix[64][64];
-//     char countsplit;
-//     char linkedname[17];
-//     // char * currfile;
-//     interrupt(0x21, 2, files, 0x101, 0);
-//     interrupt(0x21, 2, files+512, 0x102, 0);
-//     while(idx < 14) {
-//         *(filenames + idx) = 0x0;
-//         idx++;
-//     }
-//     clear(output,100);
-//     pathIdx = getFilePathIdx(parentIdx, filenames);
-//     countsplit = strsplit(filenames, '/', filematrix);
+void cat(char * filenames, unsigned char parentIdx)
+{
+    char files[1024];
+    char buff[512 * 16];
+    int idx = strlen(filenames);
+    int pathIdx;
+    char output[100];
+    char filematrix[64][64];
+    char countsplit;
+    char linkedname[17];
+    // char * currfile;
+    interrupt(0x21, 2, files, 0x101, 0);
+    interrupt(0x21, 2, files+512, 0x102, 0);
+    while(idx < 14) {
+        *(filenames + idx) = 0x0;
+        idx++;
+    }
+    clear(output,100);
+    pathIdx = getFilePathIdx(parentIdx, filenames);
+    countsplit = strsplit(filenames, '/', filematrix);
 
-//     if(files[0x10*pathIdx+1]<0x20){
-//         if(pathIdx>=0)
-//         {
-//             interrupt(0x21, 4 + 0x100*files[0x10*pathIdx], buff, filematrix[countsplit-1], 0);
-//             interrupt(0x21, 0, buff , 0, 0);
-//             interrupt(0x21, 0, "\r\n" , 0, 0);
-//             return;
-//         }
-//     } else{
-//         if(pathIdx>=0)
-//         {
-//             strslice(files+0x10*(files[0x10*pathIdx+1]-0x20),linkedname,2,16);
-//             interrupt(0x21, 4 + 0x100*(files[0x10*(files[0x10*pathIdx+1]-0x20)]), buff, linkedname, 0);
-//             interrupt(0x21, 0, buff , 0, 0);
-//             interrupt(0x21,0,"\r\n", 0,0);
-//             return;
-//         }       
-//     }
-//     interrupt(0x21, 0, "file tidak ditemukan\r\n", 0, 0);
-// }
+    if(files[0x10*pathIdx+1]<0x20){
+        if(pathIdx>=0)
+        {
+            interrupt(0x21, 4 + 0x100*files[0x10*pathIdx], buff, filematrix[countsplit-1], 0);
+            interrupt(0x21, 0, buff , 0, 0);
+            interrupt(0x21, 0, "\r\n" , 0, 0);
+            return;
+        }
+    } else{
+        if(pathIdx>=0)
+        {
+            strslice(files+0x10*(files[0x10*pathIdx+1]-0x20),linkedname,2,16);
+            interrupt(0x21, 4 + 0x100*(files[0x10*(files[0x10*pathIdx+1]-0x20)]), buff, linkedname, 0);
+            interrupt(0x21, 0, buff , 0, 0);
+            interrupt(0x21,0,"\r\n", 0,0);
+            return;
+        }       
+    }
+    interrupt(0x21, 0, "file tidak ditemukan\r\n", 0, 0);
+}
--- src/shell.c
+++ src/shell.c
@@ -4,8 +4,6 @@
 
 void cwd(int pathIdx,char *dir);
 
-void cat(char * filenames,unsigned char dir);
-
 int cd(int currParentIdx, char *dirPath);
 
 void ls(unsigned char parentIndex);
@@ -55,9 +53,7 @@ int main(){
             }
         } else if(strcmp(command[0],"ls",strlen(command[0])) && strlen(command[0])==2){
             ls(parentIdx);
-        } else if(strcmp(command[0],"cat",strlen(command[0])) && strlen(command[0])==3){
-            cat(command[1],parentIdx);
-        } 
+        }
         else{
             messageArguments(input,parentIdx);
             interrupt(0x21,0x0006,command[0],0x3000,execStatus);
@@ -254,46 +250,4 @@ void messageArguments(char *argv,char parentIndex){
     }
 
     interrupt(0x21,(i << 8) + 0x5,arg,"~temp",&trash);
-}
-
-void cat(char * filenames, unsigned char parentIdx)
-{
-    char files[1024];
-    char buff[512 * 16];
-    int idx = strlen(filenames);
-    int pathIdx;
-    char output[100];
-    char filematrix[64][64];
-    char countsplit;
-    char linkedname[17];
-    // char * currfile;
-    interrupt(0x21, 2, files, 0x101, 0);
-    interrupt(0x21, 2, files+512, 0x102, 0);
-    while(idx < 14) {
-        *(filenames + idx) = 0x0;
-        idx++;
-    }
-    clear(output,100);
-    pathIdx = getFilePathIdx(parentIdx, filenames);
-    countsplit = strsplit(filenames, '/', filematrix);
-
-    if(files[0x10*pathIdx+1]<0x20){
-        if(pathIdx>=0)
-        {
-            interrupt(0x21, 4 + 0x100*files[0x10*pathIdx], buff, filematrix[countsplit-1], 0);
-            interrupt(0x21, 0, buff , 0, 0);
-            interrupt(0x21, 0, "\r\n" , 0, 0);
-            return;
-        }
-    } else{
-        if(pathIdx>=0)
-        {
-            strslice(files+0x10*(files[0x10*pathIdx+1]-0x20),linkedname,2,16);
-            interrupt(0x21, 4 + 0x100*(files[0x10*(files[0x10*pathIdx+1]-0x20)]), buff, linkedname, 0);
-            interrupt(0x21, 0, buff , 0, 0);
-            interrupt(0x21,0,"\r\n", 0,0);
-            return;
-        }       
-    }
-    interrupt(0x21, 0, "file tidak ditemukan\r\n", 0, 0);
 }
